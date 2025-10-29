2c9ff15b2b4ead10138aabf233cfd2d16d6a3247
AdeptR <truongmanhnguyena@gmail.com>
2025-10-29 22:06:06 +0700

    update lab 2

--- LAB2/Exercise1/vite.config.ts
+++ LAB2/Exercise1/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "./",
+  root: ".",
   build: {
     outDir: "dist",
-    sourcemap: true,
-  },
-  server: {
-    port: 3000,
-  },
-  esbuild: {
-    jsxFactory: "createElement",
-    jsxFragment: "Fragment",
   },
 });
