67951349194ad020fcd67d474f32ee53a05000e1
AdeptR <truongmanhnguyena@gmail.com>
2025-11-11 21:03:36 +0700

    up ex 2 + 3

---
+++ LAB3/react-basics-exercise/src/UserProfile.jsx
@@ -0,0 +1,24 @@
+function UserProfile() {
+      const user = {
+        name: "Adept Truong",
+        email: "adept.truong@example.com",
+        avatarUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+        imageSize: 90,
+      };
+  return (
+    <>
+      <h2>User Profile</h2>
+      <img
+        className="profile-avatar"
+        src={user.avatarUrl}
+        alt={user.name}
+        width={user.imageSize}
+        height={user.imageSize}
+      />
+      <p>Name: Trương Mạnh Nguyên</p>
+      <p>Email: truongmanhnguyen@example.com</p>
+    </>
+  );
+}
+
+export default UserProfile;
